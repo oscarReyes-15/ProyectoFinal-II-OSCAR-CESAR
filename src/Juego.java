966ac9b8585e40c5f148e8c1dacfcec926b9c5cf
oscarReyes-15 <oscar.andres.0815@gmail.com>
2025-02-25 12:45:51 -0600

    estructura de paquetes

--- src/Juego.java
+++ src/Juego.java
@@ -2,12 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package sodokanv1;
 
 /**
  *
- * @author danilos
+ * @author LENOVO
  */
-public class Player {
+public class Juego {
     
 }
